14a72289ea0a89a3419044c032775a8e80b1a946
unique123 <balukuppili123@gmail.com>
2025-07-14 23:28:09 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "balu1323.TF-Ansible.project.bucket"
+  bucket = "balu1323.tf-ansible.project.bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
