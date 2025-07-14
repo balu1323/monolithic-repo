6d664fea3aaca9109301e5f2602796dbb01ed08f
unique123 <balukuppili123@gmail.com>
2025-07-14 23:43:48 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 
-#terraform {
-#backend "s3" {
-#region = "us-east-1"
-#bucket = "balu1323.TF-Ansible.project.bucket"
-#key = "prod/terraform.tfstate"
-#}
-#}
+terraform {
+backend "s3" {
+region = "us-east-1"
+bucket = "balu1323.TF-Ansible.project.bucket"
+key = "prod/terraform.tfstate"
+}
+}
