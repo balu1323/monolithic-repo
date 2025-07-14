1f5efd561d3770c9ef55990fcd7f159e699a7e4d
unique123 <balukuppili123@gmail.com>
2025-07-14 21:55:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = ""
+    image_id           = "ami-050fd9796aa387c0d"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t2.micro"
     key_name = "DevOpsNewKP"
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-", "subnet-"]
+     subnets = ["subnet-0b2e8c10af88eedc7", "subnet-02611d5765d0974a7"]
      listener {
       instance_port     = 80
       instance_protocol = "http"
@@ -33,7 +33,7 @@ resource "aws_autoscaling_group" "web_server_asg" {
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["us-east-1", "us-east-1"] 
+    availability_zones    = ["us-east-1f", "us-east-1c"] 
     launch_template {
         id      = aws_launch_template.web_server_as.id
         version = "$Latest"
